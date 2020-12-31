329d842532911d963c6fbea0f5b03f22877276ab
Your Name <you@example.com>
2020-12-30 23:52:22 -0300

    Actualizacion actividad Pacman

--- Games/Pacman/funciones.js
+++ Games/Pacman/funciones.js
@@ -1,25 +1,41 @@
 var mundo = [
-    [2,2,2,2,2,2,2,2,2,2,2],
-    [2,0,1,1,1,2,1,1,1,1,2],
-    [2,1,2,1,1,1,1,1,2,1,2],
-    [2,1,2,1,2,1,2,1,2,1,2],
-    [2,1,1,1,2,0,2,1,1,1,2],
-    [2,1,1,1,2,2,2,1,3,1,2],
-    [2,1,2,1,1,1,1,1,2,1,2],
-    [2,1,2,1,2,2,2,1,2,1,2],
-    [2,1,1,1,1,3,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2,2]
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,2,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,2,2],
+    [2,1,1,2,1,2,1,2,1,1,2,1,1,2,1,2,1,2,1,1,2],
+    [2,1,2,2,1,2,1,2,1,1,2,1,1,2,1,2,1,2,2,1,2],
+    [2,1,1,1,3,2,1,2,1,1,2,1,1,2,1,2,1,1,1,1,2],
+    [2,1,2,2,2,2,1,2,1,1,1,1,1,2,1,2,2,2,2,1,2],
+    [2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,2,2,1,2,2,2,1,2,0,0,0,2,1,2,2,2,1,2,2,2],
+    [2,1,1,1,1,1,1,1,2,0,0,0,2,1,1,1,1,3,1,1,2],
+    [2,2,2,1,2,2,2,1,2,2,2,2,2,1,2,2,2,1,2,2,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,1,1,2],
+    [2,1,2,2,2,2,1,2,1,1,0,1,1,2,1,2,2,2,2,1,2],
+    [2,1,1,1,1,2,1,2,1,1,2,1,1,2,1,2,1,1,1,1,2],
+    [2,1,2,2,1,2,1,2,1,1,2,1,1,2,1,2,1,2,2,1,2],
+    [2,1,3,2,1,2,1,2,1,1,2,1,1,2,1,2,1,2,1,1,2],
+    [2,2,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,2,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+
 ], pacman = {
     id: "pacman",
-    x: 1,
-    y: 1
+    x: 13,
+    y: (mundo.length/2).toFixed()
 }, pacman2 = {
     id: "pacman2",
-    x: 8,
+    x: 17,
     y: mundo.length-1
 }, fantasma = {
-    x: 4,
-    y: mundo.length/2
+    x: 9,
+    y: (mundo.length/2).toFixed()
+}, fantasma2 = {
+    x: 9,
+    y: ((mundo.length/2)+1).toFixed()
+}, fantasma3 = {
+    x: 9,
+    y: ((mundo.length/2)-1).toFixed()
 }, puntos = 0, vidas = 2;
 
 function mostrarMundo(){
@@ -56,6 +72,12 @@ function mostrarPacman(){
 function mostrarFantasma(){
     document.getElementById("fantasma").style.top = fantasma.x*20+"px";
     document.getElementById("fantasma").style.left = fantasma.y*20+"px";
+
+    document.getElementById("fantasma2").style.top = fantasma2.x*20+"px";
+    document.getElementById("fantasma2").style.left = fantasma2.y*20+"px";
+
+    document.getElementById("fantasma3").style.top = fantasma3.x*20+"px";
+    document.getElementById("fantasma3").style.left = fantasma3.y*20+"px";
 }
 function mostrarPuntos(){
     document.getElementById("puntos").innerHTML = puntos;
@@ -72,17 +94,33 @@ function mostrarVidas(){
     }  
     document.getElementById("vida").innerHTML = salida;
 }
-
+// Cargar mundo y elementos de interaccion 
 mostrarMundo();
 mostrarPacman();
 mostrarFantasma();
 mostrarPuntos();
 mostrarVidas();
 
-//Movimientos por teclado PacMan 
+function puntaje(pos,px,py){
+    if(pos == 1){
+        puntos +=10;
+        mundo[px][py] = 0;
+        mostrarMundo();
+        mostrarPuntos();
+    }
+    else if(pos == 3){
+        puntos +=50;
+        mundo[px][py] = 0;
+        mostrarMundo();
+        mostrarPuntos();
+    }
+}
+
+// Movimientos por teclado PacMan 
 document.onkeydown = function(pm){
-    //KeyCode 37 izquierda, 39 derecha, 38 arriba, 40 abajo
-    //Pacman
+    var posicion = 0, posX = 0, posY = 0;
+    // KeyCode: 37 izquierda, 39 derecha, 38 arriba, 40 abajo
+    // Pacman
     if(pm.keyCode == 37 && mundo[pacman.x][pacman.y-1] !=2){
         pacman.y--;
         document.getElementById(pacman.id).style.transform = "rotate(180deg)";
@@ -99,9 +137,11 @@ document.onkeydown = function(pm){
         pacman.x++;
         document.getElementById(pacman.id).style.transform = "rotate(90deg)";
     }
-
-    //KeyCode: 87 W, 65 A, 83 S, 68 D
-    //pacman2
+    posicion = mundo[pacman.x][pacman.y];
+    posX = pacman.x;
+    posY = pacman.y;
+    // KeyCode: 87 W, 65 A, 83 S, 68 D
+    // pacman2
     if(pm.keyCode == 65 && mundo[pacman2.x][pacman2.y-1] !=2){
         pacman2.y--;
         document.getElementById(pacman2.id).style.transform = "rotate(180deg)";
@@ -118,23 +158,12 @@ document.onkeydown = function(pm){
         pacman2.x++;
         document.getElementById(pacman2.id).style.transform = "rotate(90deg)";
     }
-
+    
     //Coordenadas PacMan
     console.log("Posicion: mundo["+pacman.x+"]["+pacman.y+"]");
     
     //Obtencion de puntos
-    if(mundo[pacman.x][pacman.y] == 1){
-        mundo[pacman.x][pacman.y] = 0;
-        puntos +=10;
-        mostrarMundo();
-        mostrarPuntos();
-    }
-    else if(mundo[pacman.x][pacman.y] == 3){
-        mundo[pacman.x][pacman.y] = 0;
-        puntos +=50;
-        mostrarMundo();
-        mostrarPuntos();
-    }    
+    puntaje(posicion,posX,posY);    
     mostrarPacman();
 }
 
@@ -151,7 +180,7 @@ function moverFantasma(){
     }
     else if(azar == 2 && mundo[fantasma.x][fantasma.y+1] !=2){
         fantasma.y++;  
-    }    
+    }   
     else if(azar == 3 && mundo[fantasma.x-1][fantasma.y] !=2){
         fantasma.x--;
     }
@@ -161,9 +190,10 @@ function moverFantasma(){
 
     //Colision con Fantasma
     if(pacman.x == fantasma.x && pacman.y == fantasma.y){
-        pacman.x = 1;
-        pacman.y = 1; 
+        pacman.x = 13;
+        pacman.y = (mundo.length/2).toFixed(); 
         document.getElementById("pacman").style.transform = "rotate(0deg)";
+        mostrarPacman();
         
         //Perdida de vidas
         if(vidas != 0){
